2bab485489349d7f2866cb86847c0c5124a8be81
destradax <destradax@gmail.com>
2016-02-17 22:02:24 -0500

    Added Workorder fixtures.

--- app/models/workorder.js
+++ app/models/workorder.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  id: DS.attr('number'),
+var Workorder = DS.Model.extend({
   dateCreated: DS.attr('date'),
   dateCompleted: DS.attr('date'),
   assignedTo: DS.attr('string'),
@@ -10,3 +9,40 @@ export default DS.Model.extend({
   status: DS.attr('string'),
   state: DS.attr('string')
 });
+
+Workorder.reopenClass({
+	FIXTURES: [
+		{
+			id: 1,
+			dateCreated: "2015-02-07T16:44:57.000Z",
+			dateCompleted: "2015-02-07T16:44:57.000Z",
+			assignedTo: "Mike Miller",
+			createdBy: "Mario",
+			priority: "TOP",
+			status: "OPEN",
+			state: "DEFAULT"
+		},
+		{
+			id: 2,
+			dateCreated: "2015-02-07T16:44:57.000Z",
+			dateCompleted: "2015-02-07T16:44:57.000Z",
+			assignedTo: "Mr Schmidth",
+			createdBy: "Luigi",
+			priority: "MID",
+			status: "OPEN",
+			state: "IDLE"
+		},
+		{
+			id: 3,
+			dateCreated: "2015-02-07T16:44:57.000Z",
+			dateCompleted: "2015-02-07T16:44:57.000Z",
+			assignedTo: "John Doe",
+			createdBy: "Toad",
+			priority: "LOW",
+			status: "CLOSED",
+			state: "DEFAULT"
+		}
+	]
+});
+
+export default Workorder;
--- app/routes/workorders.js
+++ app/routes/workorders.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return ["Workorder 1", "Workorder 2", "Workorder 3", "Workorder 4", "Workorder 5"];
+		return this.store.findAll("workorder");
 	}
 });
